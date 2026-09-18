0792b731f40cb0841e3c3a436be09cfe1a16a305
il0nza <ilonzaguides@gmail.com>
2026-09-18 15:02:58 +0500

    practice1

--- test/Main.java
+++ test/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("hello");
+    void printTable() {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        // int, boolean, double, char - primitive
+        // String, Arrays, List - reference
+
+        Main obj = new Main();
+        obj.printTable();
+
+        // String str = new String("String");
+        // System.out.println(str);
+        // System.out.println(args[0]);
+
+        System.out.print("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
+        sc.close(); 
     }
 }
-
-
